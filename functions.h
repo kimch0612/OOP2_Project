7ee7bcbcdc412bde568fe0bc8c570ea7edc018c5
Changhwan Kim <kimch061279@gmail.com>
2023-09-26 14:16:04 +0900

    add file; functions.h

---
+++ functions.h
@@ -0,0 +1,12 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <windows.h>
+using namespace std;
+int dice_gen();
+char score_section();
+char dice_section();
+char final_score();
+#endif
