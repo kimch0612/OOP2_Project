26c88d08294b3e66a5e5a6174e33d784b9d60940
Changhwan Kim <kimch061279@gmail.com>
2023-09-26 13:16:29 +0900

    edit mainwindow.cpp file; change titlebar icon

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -5,6 +5,7 @@ MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
+    setWindowIcon(QIcon("C:/Users/Chals/Documents/OOP2_Project/Yahtzee/icon.png"));
     setStyleSheet("background-color:blck");
     QMainWindow::setWindowFlags( Qt::WindowTitleHint |  Qt::WindowMinimizeButtonHint | Qt::WindowCloseButtonHint );
     ui->setupUi(this);
@@ -14,4 +15,3 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
-
