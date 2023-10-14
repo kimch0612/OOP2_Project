f7da19bb2a236a3f3b0e89316cedcb4d4c7b1993
Changhwan Kim <kimch061279@gmail.com>
2023-10-14 19:38:31 +0900

    edit functions.cpp file; fix typo

--- functions.cpp
+++ functions.cpp
@@ -10,7 +10,7 @@ int dice_gen(void)
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_int_distribution<int> dis(1, 6);
-    return dis(gen)
+    return dis(gen);
 }
 
 void reset_dice(int *index, char **reroll_flag)
