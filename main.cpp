d254a1fe57bf0c84970356c7b4c19d105d673273
Changhwan Kim <kimch061279@gmail.com>
2023-10-21 16:29:05 +0900

    edit main.cpp file; fixed windows size

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.setWindowTitle("Yahtzee Dice");
+    w.setFixedSize(850, 460);
     w.show();
     return a.exec();
 }
