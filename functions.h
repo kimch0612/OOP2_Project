14b5fa7487996452c4207b9654e4efacf96e5a14
Changhwan Kim <kimch061279@gmail.com>
2023-11-26 12:05:18 +0900

    edit functions.h; add class

--- functions.h
+++ functions.h
@@ -12,6 +12,62 @@
 #include <string>
 #include <windows.h>
 using namespace std;
+class One{
+public:
+    int one(void)
+    {
+        for (i=0; i<5; i++) if (current_dice[i] == 1) 
+            temp += current_dice[i];
+    }
+}
+class Two : public One{
+public:
+    int two(void)
+    {
+        for (i=0; i<5; i++) if (current_dice[i] == 2)
+            temp += current_dice[i];
+    }
+}
+class Three : public Two{
+public:
+    int three(void)
+    {
+        for (i=0; i<5; i++) if (current_dice[i] == 3)
+            temp += current_dice[i];
+    }
+}
+class Four : public Three{
+public:
+    int four(void)
+    {
+        for (i=0; i<5; i++) if (current_dice[i] == 4)
+            temp += current_dice[i];
+        return temp;
+    }
+}
+class Five : public Four{
+public:
+    int five(void)
+    {
+        for (i=0; i<5; i++) if (current_dice[i] == 5)
+            temp += current_dice[i];
+    }
+}
+class Six : public Five{
+public:
+    int six(void)
+    {
+        for (i=0; i<5; i++) if (current_dice[i] == 6)
+            temp += current_dice[i];
+    }
+}
+class Bonus : public Six{
+public:
+    int bonus(void)
+    {
+        all += 35;
+    }
+}
 class score_template{
 public:
     int all, bonus, chance, fourkind, digits;
